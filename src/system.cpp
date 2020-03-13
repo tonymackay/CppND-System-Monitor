3bd39180a01dff1e392d3c075e20074c8bea9108
Tony Mackay <58574743+tonymackay@users.noreply.github.com>
2020-03-13 15:23:16 +0000

    Implement OperatingSystem function

--- src/system.cpp
+++ src/system.cpp
@@ -27,8 +27,9 @@ std::string System::Kernel() {
 // TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return 0.0; }
 
-// TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+std::string System::OperatingSystem() { 
+    return LinuxParser::OperatingSystem(); 
+}
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
